8640e6d7e4d329dda641f068a7a49aa9eb61a2f0
FMGrabner <friedrich.grabner16@imperial.ac.uk>
2017-08-22 15:56:29 +0100

    Rewrote the XSLT transform stuffs

--- report/to-do-list.py
+++ report/to-do-list.py
@@ -13,17 +13,17 @@ General comments:
 
 
 Specific comments:
-  Intro: The primary aim of the Nektar++ libraries is to encapsulate the spectral/hp element method, upon which various PDE solvers for simulating different physical phenomena can be built.
+  # Intro: The primary aim of the Nektar++ libraries is to encapsulate the spectral/hp element method, upon which various PDE solvers for simulating different physical phenomena can be built.
 
-  Intro: "rich text-editors" -> "Graphical User Interfaces" (e.g. your OpenFOAM example!)
+  # Intro: "rich text-editors" -> "Graphical User Interfaces" (e.g. your OpenFOAM example!)
   
-  Aim: Be specific: graphical/web-based interface, allows user to specify problem to solve in an intuitive manner, generates a syntactically valid XML free from logical errors (prevents selection of incompatible  unctionality, rather than a simulation which is necessarily numerically stable, for example).
+  # Aim: Be specific: graphical/web-based interface, allows user to specify problem to solve in an intuitive manner, generates a syntactically valid XML free from logical errors (prevents selection of incompatible  unctionality, rather than a simulation which is necessarily numerically stable, for example).
   
-  Objective 1: functionality and parameter space
+  # Objective 1: functionality and parameter space
   
-  Objectives: "functions" -> "simulation parameters"? Does it make sense to call the Reynolds number a "function"?
+  # Objectives: "functions" -> "simulation parameters"? Does it make sense to call the Reynolds number a "function"?
   
-  P5 XML Structure: multiply -> multiple
+  # P5 XML Structure: multiply -> multiple
   
   Figure 3.2: Only some of these boxes ever seem to be discussed in the text? Not clear what you mean by "TemPSS file dependency" (see general comment above on figure captions!). Where does the populated profile fit in to this?
   
@@ -31,21 +31,21 @@ Specific comments:
   
   Feel free to use inline code (formatted as monospace) to be more precise, e.g. "During transformation the <xsl:if ...> tag performs a test...", "The <xsl:apply-template> evaluates and substitutes the template which matches the given 'select' and 'mode'".
   
-  P8: "line 41, of figure 3.8", but lines go from 1-6.
+  # P8: "line 41, of figure 3.8", but lines go from 1-6.
   
-  P10: "illucidate" -> "elicudate".
+  # P10: "illucidate" -> "elicudate".
   
   Problem Specification: Discuss geometry loading, BC generation etc? I seemed to remember you improved an aspect of this? You should write what you did.
 
   Fig 3.11: These look rather basic and do not show off the full scope of the parameter space they encapsulate! They are also not referenced in the text. Consider removing, and reference a full list of parameters in the Appendix?
   
-  Eq 4.4: We typically use \mathbf{u} for the overall flow, \mathbf{U} for the base flow and \mathbf{u'} for the perturbation, so this should probably be: \mathbf{u} = \mathbf{U} + \epsilon\mathbf{u'}
+  # Eq 4.4: We typically use \mathbf{u} for the overall flow, \mathbf{U} for the base flow and \mathbf{u'} for the perturbation, so this should probably be: \mathbf{u} = \mathbf{U} + \epsilon\mathbf{u'}
 
-  Eq 4.5: \mathbf{u} -> \mathbf{U}, also \nabla\cdot\mathbf{u'} = 0
+  # Eq 4.5: \mathbf{u} -> \mathbf{U}, also \nabla\cdot\mathbf{u'} = 0
   
-  Eq 4.6: same as above, specifically say "steady linearised N-S"...
+  # Eq 4.6: same as above, specifically say "steady linearised N-S"...
   
-  "Unsteady Navier-Stokes"  this is the base case and repeats Eq 4.1? Maybe just add this subtitle after "4.1.4..."
+  # "Unsteady Navier-Stokes"  this is the base case and repeats Eq 4.1? Maybe just add this subtitle after "4.1.4..."
   
   Test cases: possibly the specific tables of parameters are not necessary and could be put in the appendix? The plots of forces, etc, are maybe a unnecessarily large? If you have time, it might be worth setting these to run for a longer FinalTime to show that the results are truly identical to the non-TemPSS case.
 
